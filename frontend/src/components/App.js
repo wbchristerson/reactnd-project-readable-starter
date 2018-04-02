6e1de0748258c8dfeff5b7a8e80b8fc38c8f3770
Will Christerson <wbchristerson@gmail.com>
2018-04-02 16:16:23 -0400

    fix: Re-load pages on route changes without having to refresh

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Route, Link } from 'react-router-dom'
+import { Route, Link, withRouter } from 'react-router-dom'
 import './App.css'
 import Main from './Main'
 import Page from './Page'
@@ -73,4 +73,4 @@ function mapStateToProps (fullState) {
   }
 }
 
-export default connect(mapStateToProps)(App)
+export default withRouter(connect(mapStateToProps)(App))
