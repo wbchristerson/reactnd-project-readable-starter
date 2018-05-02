321ed28412815072d34e256c35de8e41abe162f7
Will Christerson <wbchristerson@gmail.com>
2018-05-02 09:45:52 -0400

    style: Rearrange post items on main page

--- frontend/src/components/Item.js
+++ frontend/src/components/Item.js
@@ -11,7 +11,11 @@ import { Card, CardHeader, CardText, CardActions, CardTitle } from 'material-ui/
 import {List, ListItem} from 'material-ui/List'
 // import CardHeader from 'material-ui/card/cardheader.js'
 // import CardText from 'material-ui/card/cardtext.js';
-import Desert from '../images/desert.jpg'
+import Desert from '../images/desert.jpg' // image credit: Jordon Steranka, Unsplash
+import KeyboardArrowUp from 'material-ui/svg-icons/hardware/keyboard-arrow-up'
+import KeyboardArrowDown from 'material-ui/svg-icons/hardware/keyboard-arrow-down'
+// import ThumbUp from 'material-ui/svg-icons/action/thumb-up'
+// import ThumbDown from 'material-ui/svg-icons/action/thumb-down'
 
 
 class Item extends Component {
@@ -35,55 +39,71 @@ class Item extends Component {
     this.props.dispatch(setId(this.props.id))
   }
 
+  // when the edit button is clicked, prevent the action of navigating to the
+  // individual post page
+  editButtonWrapper = (event) => {
+    event.preventDefault()
+    this.edit()
+  }
+
+  // like with the editButtonWrapper function, when the destroy button is
+  // clicked, prevent the action of navigating to the individuaal post page or
+  // to the page stating that the post does not exist
+  destroyButtonWrapper = (event, id) => {
+    event.preventDefault()
+    this.destroy(id)
+  }
+
   render() {
     let link = `/${this.props.category}/${this.props.id}`
     let authorInfo = "Author: " + this.props.author
     let commentInfo = "Comments: " + this.props.commentCount.toString()
     let categoryInfo = "Category: " + this.props.category
+    // <RaisedButton label="Vote Up" onClick={() => this.vote(this.props.id, 'upVote')} />
+    // <RaisedButton label="Vote Down" onClick={() => this.vote(this.props.id, 'downVote')} />
     return (
-      <Card
-        style={{
-          background: 'linear-gradient(red, yellow)',
-          boxShadow: `1px 3px 1px #9E9E9E`,
-          marginBottom: 5
-        }}>
-        <div className="main-post-info">
-          <Link onClick={() => this.props.dispatch(setPath('/post'))} className="left-infto" to={link}>
-            <CardHeader
-              title={authorInfo}
-              subtitle={commentInfo}
-              avatar={Desert}
-              style={{
-                marginBottom: 0,
-                paddingBottom: 0,
-              }}
-            />
-            <CardTitle
-              title={this.props.title}
-              subtitle={categoryInfo}
-              style={{
-                marginTop: 0,
-                paddingTop: 0,
-              }}
-            />
-          </Link>
-          <List>
-            <ListItem>
-              <RaisedButton label="Vote Up" onClick={() => this.vote(this.props.id, 'upVote')} />
-            </ListItem>
-            <ListItem>
-              {this.props.voteScore}
-            </ListItem>
-            <ListItem>
-              <RaisedButton label="Vote Down" onClick={() => this.vote(this.props.id, 'downVote')} />
-            </ListItem>
-          </List>
-        </div>
-        <CardActions>
-          <RaisedButton label="Edit" onClick={() => this.edit()} />
-          <RaisedButton label="Delete" onClick={() => this.destroy(this.props.id)} />
-        </CardActions>
-      </Card>
+      <Link onClick={() => this.props.dispatch(setPath('/post'))} to={link}>
+        <Card
+          style={{
+            background: 'linear-gradient(red, yellow)',
+            boxShadow: `1px 3px 1px #9E9E9E`,
+            marginBottom: 5,
+          }}>
+          <div className="all-post-info">
+            <div className="main-post-info">
+              <CardHeader
+                title={authorInfo}
+                subtitle={commentInfo}
+                avatar={Desert}
+                style={{
+                  marginBottom: 0,
+                  paddingBottom: 0,
+                }}
+              />
+              <div className="sub-information">
+                <div className="like-section" onClick={(event) => {event.preventDefault()}}>
+                  <KeyboardArrowUp onClick={() => this.vote(this.props.id, 'upVote')}/>
+                  <div className="score-text">{this.props.voteScore}</div>
+                  <KeyboardArrowDown onClick={() => this.vote(this.props.id, 'downVote')}/>
+                </div>
+                <CardTitle
+                  title={this.props.title}
+                  subtitle={categoryInfo}
+                  style={{
+                    marginTop: 0,
+                    paddingTop: 0,
+                  }}
+                />
+              </div>
+            </div>
+            <div className="action-buttons">Hi</div>
+          </div>
+          <CardActions>
+            <RaisedButton label="Edit" onClick={(event) => this.editButtonWrapper(event)} />
+            <RaisedButton label="Delete" onClick={(event) => this.destroyButtonWrapper(event, this.props.id)} />
+          </CardActions>
+        </Card>
+      </Link>
     );
   }
 }
